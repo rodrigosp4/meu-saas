89da740ffdf0631f2f29baa78a35769d44c24a53
gama4x4 <contatogama4x4@gmail.com>
2026-03-04 16:43:37 -0300

    alteraçao

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,16 @@
 import app from './app.js';
 import { config } from './config/env.js';
 
+process.on('uncaughtException', (err) => {
+  console.error('💥 UNCAUGHT EXCEPTION:', err.message, err.stack);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('💥 UNHANDLED REJECTION:', reason);
+  process.exit(1);
+});
+
 // Inicia os workers BullMQ junto com o servidor
 import './workers/sync.worker.js';
 
